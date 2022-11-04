1438bb6ed13092cb6c2c884dda5c623f4b4da40c
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-11-04 22:00:46 +0800

    init

---
+++ marshal.go
@@ -0,0 +1,41 @@
+package openxml
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+	"unsafe"
+)
+
+var selfclosere = regexp.MustCompile(`></[\w:\s]+>`)
+
+// Marshal from v but use tags of t
+func Marshal(v, t any) ([]byte, error) {
+	tmp := t
+	*(*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&tmp), unsafe.Sizeof(uintptr(0)))) = *(*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&v), unsafe.Sizeof(uintptr(0))))
+	tn := t.(fmt.Stringer).String()
+	data, err := xml.Marshal(tmp)
+	if err != nil {
+		return nil, err
+	}
+	nm := reflect.ValueOf(t).Type().String()
+	nm = nm[strings.LastIndex(nm, ".")+1:]
+	return selfclosere.ReplaceAll(bytes.ReplaceAll(data, []byte(nm), []byte(tn)), []byte("/>")), nil
+}
+
+// MarshalIndent from v but use tags of t
+func MarshalIndent(v, t any, prefix, indent string) ([]byte, error) {
+	tmp := t
+	*(*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&tmp), unsafe.Sizeof(uintptr(0)))) = *(*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&v), unsafe.Sizeof(uintptr(0))))
+	tn := t.(fmt.Stringer).String()
+	data, err := xml.MarshalIndent(tmp, prefix, indent)
+	if err != nil {
+		return nil, err
+	}
+	nm := reflect.ValueOf(t).Type().String()
+	nm = nm[strings.LastIndex(nm, ".")+1:]
+	return selfclosere.ReplaceAll(bytes.ReplaceAll(data, []byte(nm), []byte(tn)), []byte("/>")), nil
+}
---
+++ marshal_test.go
@@ -0,0 +1,73 @@
+package openxml
+
+import (
+	"encoding/xml"
+	"fmt"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+type ppr struct {
+	WrPr struct {
+		WrFonts struct {
+			Wascii    string `xml:"ascii,attr"`
+			WeastAsia string `xml:"eastAsia,attr"`
+			WhAnsi    string `xml:"hAnsi,attr"`
+			Wcs       string `xml:"cs,attr"`
+			Whint     string `xml:"hint,attr"`
+		} `xml:"rFonts"`
+		Wsz struct {
+			Wval string `xml:"val,attr"`
+		} `xml:"sz"`
+		WszCs struct {
+			Wval string `xml:"val,attr"`
+		} `xml:"szCs"`
+	} `xml:"rPr"`
+	Wt string `xml:"t"`
+}
+
+type pprm struct {
+	WrPr struct {
+		WrFonts struct {
+			Wascii    string `xml:"w:ascii,attr"`
+			WeastAsia string `xml:"w:eastAsia,attr"`
+			WhAnsi    string `xml:"w:hAnsi,attr"`
+			Wcs       string `xml:"w:cs,attr"`
+			Whint     string `xml:"w:hint,attr"`
+		} `xml:"w:rFonts"`
+		Wsz struct {
+			Wval string `xml:"w:val,attr"`
+		} `xml:"w:sz"`
+		WszCs struct {
+			Wval string `xml:"w:val,attr"`
+		} `xml:"w:szCs"`
+	} `xml:"w:rPr"`
+	Wt string `xml:"w:t"`
+}
+
+func (*pprm) String() string {
+	return "w:r"
+}
+
+func TestMarshal(t *testing.T) {
+	org := []byte(`<w:r>
+	<w:rPr>
+		<w:rFonts w:ascii="华文黑体" w:eastAsia="华文黑体" w:hAnsi="华文黑体" w:cs="华文黑体" w:hint="eastAsia"/>
+		<w:sz w:val="36"/>
+		<w:szCs w:val="44"/>
+	</w:rPr>
+	<w:t>Test文本원본</w:t>
+</w:r>`)
+	m := ppr{}
+	err := xml.Unmarshal(org, &m)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(m)
+	data, err := MarshalIndent(&m, (*pprm)(nil), "", "\t")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, string(org), string(data))
+}
